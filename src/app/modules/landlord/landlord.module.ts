ccee4bb30ae757f9848e171d9b37d41d96abdcf0
nixonchebii <chebiinixon91@gmail.com>
2020-01-28 05:06:19 -0500

    Role based authentication

--- src/app/modules/landlord/landlord.module.ts
+++ src/app/modules/landlord/landlord.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandlordRoutingModule } from './landlord-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
-    declarations: [],
+    declarations: [DashboardComponent],
     imports: [CommonModule, LandlordRoutingModule]
 })
 export class LandlordModule {}
--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -3,59 +3,78 @@ import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { MatSnackBar } from '@angular/material';
 import { Router } from '@angular/router';
+import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
+  constructor(
+    private afa: AngularFireAuth,
+    private afs: AngularFirestore,
+    private snackBar: MatSnackBar,
+    private router: Router
+  ) {}
 
-  constructor(private afa: AngularFireAuth,
-     private afs: AngularFirestore,
-     private snackBar: MatSnackBar,
-     private router:Router) { }
-
-
-  registerUser(name, email, password, phone){
-    this.afa.auth.createUserWithEmailAndPassword(email,password).then((authUser) => {
-
-      this.createUser(authUser.user.uid, name, email, phone)
-    })
+  registerUser(name, email, password, phone) {
+    this.afa.auth
+      .createUserWithEmailAndPassword(email, password)
+      .then(authUser => {
+        this.createUser(authUser.user.uid, name, email, phone);
+      });
   }
 
-  createUser(uid, name, email, phone){
+  createUser(uid, name, email, phone) {
     this.afs.doc(`users/${uid}`).set({
-      name:name,
-      uid:uid,
-      email:email,
-      phone:phone,
-      roles:{
-        tenant:true,
-        agent:false,
-        landlord:false
+      name: name,
+      uid: uid,
+      email: email,
+      phone: phone,
+      roles: {
+        tenant: true,
+        agent: false,
+        landlord: false
       }
-    })
-    this.showSnackBar()
+    });
+    this.showSnackBar();
   }
-  showSnackBar(){
-    this.openSnackBar("Registered Successfully!", "Ok")
-    }
-    openSnackBar(message: string, action: string) {
-      this.snackBar.open(message, action, {
-        duration: 2000,
-        // here specify the position
-        verticalPosition: 'top'
+  showSnackBar() {
+    this.openSnackBar('Registered Successfully!', 'Ok');
+  }
+  openSnackBar(message: string, action: string) {
+    this.snackBar.open(message, action, {
+      duration: 2000,
+      // here specify the position
+      verticalPosition: 'top'
+    });
+  }
+
+  userLogin(email, password) {
+    this.afa.auth.signInWithEmailAndPassword(email, password).then(authUser => {
+      this.getCurrentUser(authUser.user.uid);
+    });
+  }
+  getCurrentUser(uid) {
+    this.afs
+      .doc<User>(`users/${uid}`)
+      .valueChanges()
+      .subscribe(user => {
+        if (user.roles.agent) {
+          this.navigateToAgentDashboard(uid);
+        } else if (user.roles.landlord) {
+          this.navigateToLandLordDashboard(uid);
+        } else if (user.roles.tenant) {
+          this.navigateToTenantDashboard(uid);
+        }
       });
   }
-  
-    userLogin(email,password){
-      this.afa.auth.signInWithEmailAndPassword(email,password).then((user) =>{
-        this.goHome();
-      });
-      }
-  goHome() {
-   this.router.navigate(['/home'])
+  navigateToLandLordDashboard(uid) {
+    this.router.navigate(['/landlord/' + { uid } + '/dashboard']);
   }
-    
-    
-  
+  navigateToAgentDashboard(uid) {
+    this.router.navigate(['/agent/' + { uid } + '/dashboard']);
+  }
+  navigateToTenantDashboard(uid) {
+    this.router.navigate(['/tenant/' + { uid } + '/dashboard']);
+  }
 }
