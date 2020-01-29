66cdd653776ab3a31b256e3be77c6b5d0d47ca77
Chess <chessbelimer@gmail.com>
2020-01-29 16:45:21 +0300

    pushed

--- src/app/modules/agent/agent-routing.module.ts
+++ src/app/modules/agent/agent-routing.module.ts
@@ -2,11 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LandlordsComponent } from './landlords/landlords.component';
-<<<<<<< HEAD
+
 import { PropertiesComponent } from './properties/properties.component';
-=======
 import { CategoryComponent } from './category/category.component';
->>>>>>> 3d9f7b24f0c61cce2f17781bdca533019d94b5f6
 
 
 const routes: Routes = [
@@ -19,13 +17,11 @@ const routes: Routes = [
     component: LandlordsComponent
   },
   {
-<<<<<<< HEAD
     path: 'properties',
-    component: PropertiesComponent
-=======
+    component: PropertiesComponent},
+    {
     path: 'categories',
     component: CategoryComponent
->>>>>>> 3d9f7b24f0c61cce2f17781bdca533019d94b5f6
   }
 ];
 
