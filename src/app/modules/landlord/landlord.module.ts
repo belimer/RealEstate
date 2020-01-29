514037b3eb285bb1920dd409bcb72c94b9382756
nixonchebii <chebiinixon91@gmail.com>
2020-01-29 01:12:42 -0500

    Landlord dashboard

--- src/app/modules/landlord/landlord.module.ts
+++ src/app/modules/landlord/landlord.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandlordRoutingModule } from './landlord-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+
 
 @NgModule({
-    declarations: [DashboardComponent],
+    declarations: [DashboardComponent, SidenavComponent],
     imports: [CommonModule, LandlordRoutingModule]
 })
 export class LandlordModule {}
