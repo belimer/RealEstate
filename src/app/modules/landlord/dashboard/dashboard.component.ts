eac8f480b471a10051d45690bf1d52945148410b
nixonchebii <chebiinixon91@gmail.com>
2020-02-12 13:29:02 -0500

    Updated landlord UI

--- src/app/modules/landlord/dashboard/dashboard.component.ts
+++ src/app/modules/landlord/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Property } from 'src/app/models/property.model';
+import { Booking } from 'src/app/models/bookings.model';
+import { LandlordsService } from 'src/app/services/landlords.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'app-dashboard',
@@ -6,7 +10,24 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: [ './dashboard.component.css' ]
 })
 export class DashboardComponent implements OnInit {
-	constructor() {}
-
-	ngOnInit() {}
-}
+    property:Property[];
+    bookings: Booking[]
+    landlordId: any
+    constructor(private landlordService:LandlordsService, private activatedRoute: ActivatedRoute) { }
+  
+    ngOnInit() {
+      this.landlordId = this.activatedRoute.snapshot.params['id']
+      this.getProperties(this.landlordId);
+     
+    }
+    getProperties(landlordId){
+      this.landlordService.getMyproperty(landlordId).subscribe(properties=>{
+        this.property = properties.map(item=>{
+          return{
+            id: item.payload.doc.id,
+            ...item.payload.doc.data()
+          }as Property
+        })
+      })
+    }
+  }
